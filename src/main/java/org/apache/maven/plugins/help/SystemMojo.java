cd27e51d9e693c59202ed82e9031348e8b40ebe0
Michael Osipov <michaelo@apache.org>
2022-12-03 01:16:45 +0100

    Prepare for Doxia 2.0.0

--- src/main/java/org/apache/maven/plugins/help/SystemMojo.java
+++ src/main/java/org/apache/maven/plugins/help/SystemMojo.java
@@ -73,20 +73,12 @@ public class SystemMojo
         message.append( StringUtils.repeat( "=", LINE_LENGTH ) ).append( LS );
         message.append( "Environment Variables" ).append( LS );
         message.append( StringUtils.repeat( "=", LINE_LENGTH ) ).append( LS );
-        try
+        Properties envVars = CommandLineUtils.getSystemEnvVars();
+        for ( String key : envVars.stringPropertyNames() )
         {
-            Properties envVars = CommandLineUtils.getSystemEnvVars();
-            for ( String key : envVars.stringPropertyNames() )
-            {
-                message.append( LS );
-                message.append( key ).append( "=" ).append( envVars.getProperty( key ) );
-            }
+            message.append( LS );
+            message.append( key ).append( "=" ).append( envVars.getProperty( key ) );
         }
-        catch ( IOException e )
-        {
-            getLog().warn( "Unable to get the environment variables: " + e.getMessage() );
-        }
-
         message.append( LS );
 
         if ( output != null )
